a421afa470dd118452d71b65460cc10e01730d77
Neeraj177 <neerajyt17@gmail.com>
2025-05-20 23:47:38 +0530

    added exception handler method

--- demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -4,12 +4,19 @@ import com.example.demo.utility.ResponseStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.FileNotFoundException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseStatus handleUserNotFoundException(UserNotFoundException userNotFoundException)
     {
         return new ResponseStatus(200,userNotFoundException.getMessage());
+    }
+    @ExceptionHandler(FileNotFoundException.class)
+    public ResponseStatus handleException(FileNotFoundException exception)
+    {
+        return new ResponseStatus(500,exception.getMessage());
     }
 
 
